61ceac208ad45497e8c430d7f7e5f9144af32a82
Ako <thisisfear@internet.ru>
2025-04-09 11:41:57 +0400

    basqa simbol ve reqem daxil etmemesin ucun yaratmisam funskiyani

--- Lyricsfunctions.h
+++ Lyricsfunctions.h
@@ -1,1 +1,9 @@
 #pragma once
+
+bool isValidNameOrAuthor(const string& input) {
+    for (size_t i = 0; i < input.length(); ++i) {
+        if ((input[i] < 65 || (input[i] > 90 && input[i] < 97) || input[i] > 122) && input[i] != ' ') {
+            return false;
+        }
+    }
+}
